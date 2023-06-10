08bb30cf23f8cd4645dbf3916a94958b56a450cd
Shahbaz Ali <shahbazalivk@gmail.com>
2023-06-11 04:29:51 +0500

    menubar responsiveness improved

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -42,7 +42,7 @@ const AppHeader = () => {
 			id="nav"
 			className="sm:container sm:mx-auto"
 		>
-			<div className="z-10 max-w-screen-lg xl:max-w-screen-xl block sm:flex sm:justify-between sm:items-center py-6">
+			<div className="z-10 max-w-screen-lg xl:max-w-screen-xl block md:flex justify-between items-center py-6">
 				{/* Header menu links and small screen hamburger menu */}
 				<div className="flex justify-between items-center px-4 sm:px-0">
 					<div>
@@ -69,7 +69,7 @@ const AppHeader = () => {
 							onClick={() => setTheme(activeTheme)}
 							aria-label="Theme Switcher"
 							style={{ padding: '0.55rem' }}
-							className="block sm:hidden ml-0 bg-primary-light dark:bg-ternary-dark shadow-sm rounded-xl cursor-pointer mr-2"
+							className="block small-screen-darkmode-icon ml-0 bg-primary-light dark:bg-ternary-dark shadow-sm rounded-xl cursor-pointer mr-2"
 						>
 							{activeTheme === 'dark' ? (
 								<FiMoon className="text-ternary-dark hover:text-gray-400 dark:text-ternary-light dark:hover:text-primary-light text-xl" />
@@ -79,7 +79,7 @@ const AppHeader = () => {
 						</div>
 
 						{/* Small screen hamburger menu */}
-						<div className="sm:hidden">
+						<div className={'small-screen-hamburger-menu'}>
 							<button
 								onClick={toggleMenu}
 								type="button"
@@ -106,7 +106,7 @@ const AppHeader = () => {
 				<div
 					className={
 						showMenu
-							? 'block m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none'
+							? 'flex flex-col m-0 md:ml-4 mt-5 sm:mt-3 md:mt-3 large-screen-open-menu p-5 md:p-0 justify-center items-start shadow-lg md:shadow-none'
 							: 'hidden'
 					}
 				>
@@ -119,22 +119,22 @@ const AppHeader = () => {
 					</Link>
 					<Link
 						to="/about"
-						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
+						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 md:border-t-0 border-primary-light dark:border-secondary-dark"
 						aria-label="About Me"
 					>
 						About Me
 					</Link>
 					<Link
 						to="/contact"
-						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
+						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 md:border-t-0 border-primary-light dark:border-secondary-dark"
 						aria-label="Contact"
 					>
 						Contact
 					</Link>
-					<div className="border-t-2 pt-3 sm:pt-0 sm:border-t-0 border-primary-light dark:border-secondary-dark">
+					<div className="border-t-2 pt-3 sm:pt-0 md:border-t-0 border-primary-light dark:border-secondary-dark">
 						<span
 							onClick={showHireMeModal}
-							className="font-general-medium sm:hidden block text-left text-md bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-sm px-4 py-2 mt-2 duration-300 w-24"
+							className="font-general-medium md:hidden block text-left text-md bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-sm px-4 py-2 mt-2 duration-300 w-24"
 							aria-label="Hire Me Button"
 						>
 							<Button title="Hire Me" />
@@ -143,7 +143,7 @@ const AppHeader = () => {
 				</div>
 
 				{/* Header links large screen */}
-				<div className="font-general-medium hidden m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none">
+				<div className={`font-general-medium hidden m-0 sm:ml-4 mt-5 sm:mt-3 md:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none`}>
 					<Link
 						to="/projects"
 						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
@@ -168,7 +168,7 @@ const AppHeader = () => {
 				</div>
 
 				{/* Header right section buttons */}
-				<div className="hidden sm:flex justify-between items-center flex-col md:flex-row">
+				<div className="hidden md:flex justify-between items-center flex-col md:flex-row">
 					<div className="hidden md:flex">
 						<span
 							onClick={showHireMeModal}
