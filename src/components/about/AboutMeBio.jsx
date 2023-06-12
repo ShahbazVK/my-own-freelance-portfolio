1fd792d49f7d46af53938ca1d85a2bcc7421147f
unknown <muhammadadilahmadsiddiqui90@gmail.com>
2023-06-12 23:30:58 +0500

    About Me Fixed

--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -8,13 +8,13 @@ const AboutMeBio = () => {
 	return (
 		<div className="block sm:flex sm:gap-10 mt-10 sm:mt-20">
 			<div className="sm:w-1/4 mb-7 sm:mb-0">
-				<img className='rounded-lg' src="https://react-tailwindcss-portfolio.netlify.app/static/media/profile.bbe2eb2e.jpeg" alt="profile-pic" />
+				<img className='rounded-lg profilePic' src="https://react-tailwindcss-portfolio.netlify.app/static/media/profile.bbe2eb2e.jpeg" alt="profile-pic" />
 			</div>
 
 			<div className="font-general-regular w-full sm:w-3/4 text-left">
 				{aboutMe.map((bio) => (
 					<p
-						className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+						className="mb-4 text-ternary-dark dark:text-ternary-light text-lg textJustify"
 						key={bio.id}
 					>
 						{bio.bio}
