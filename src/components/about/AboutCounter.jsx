9b1b6d6001f1e97899df8fc354090b4e07639fbd
Shahbaz Ali <shahbazalivk@gmail.com>
2023-06-25 19:26:12 +0500

    logo and about me usecountup improved

--- src/components/about/AboutCounter.jsx
+++ src/components/about/AboutCounter.jsx
@@ -1,11 +1,31 @@
 import { useCountUp } from 'react-countup';
 import CounterItem from './CounterItem';
+import { useEffect, useState } from 'react';
 
 const AboutCounter = () => {
-	useCountUp({ ref: 'experienceCounter', end: 9, duration: 2 });
-	useCountUp({ ref: 'githubStarsCounter', end: 20, duration: 2 });
+	function calculateYearsOfCoding() {
+		var startDate = new Date(2014, 0, 1); // January is month 0 in JavaScript
+		var currentDate = new Date();
+
+		var totalMonths = (currentDate.getFullYear() - startDate.getFullYear()) * 12 +
+			(currentDate.getMonth() - startDate.getMonth());
+
+		var years = Math.floor(totalMonths / 12);
+		var months = (totalMonths % 12).toFixed(0);
+
+		return years + '.' + months;
+	}
+	useCountUp({ ref: 'experienceCounter', end: calculateYearsOfCoding(), decimals: 1, decimal: "." });
+	useCountUp({ ref: 'githubStarsCounter', end: localStorage.getItem('repos'), duration: 2 });
 	useCountUp({ ref: 'feedbackCounter', end: 92, duration: 2 });
 	useCountUp({ ref: 'projectsCounter', end: 77, duration: 2 });
+	useEffect(() => {
+		fetch('https://api.github.com/users/ShahbazVK')
+			.then(response => response.json())
+			.then(data => {
+				localStorage.setItem('repos', data.public_repos)
+			})
+	}, [])
 
 	return (
 		<div className="mt-10 sm:mt-20 bg-primary-light dark:bg-ternary-dark shadow-sm">
@@ -17,9 +37,9 @@ const AboutCounter = () => {
 				/>
 
 				<CounterItem
-					title="Stars on GitHub"
+					title="Repositories on GitHub"
 					counter={<span id="githubStarsCounter" />}
-					measurement="k+"
+				// measurement="+"
 				/>
 
 				<CounterItem
